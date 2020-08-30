43a888e7b64c6d2a2e323f6ed94967a43126d643
김재구 <rlaworn1993@gmail.com>
2020-08-30 21:38:38 +0900

    Create DepartmentHighestSalary.sql

---
+++ DepartmentHighestSalary.sql
@@ -0,0 +1,18 @@
+# GROUP BY 절은 데이터들을 원하는 그룹으로 나눌 수 있다.
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+	)
+;
