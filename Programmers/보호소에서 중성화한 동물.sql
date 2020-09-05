6de4c87e42c03a93ed6f3aa421dc51cf6116d9b0
김재구 <rlaworn1993@gmail.com>
2020-09-05 22:14:28 +0900

    09.05

---
+++ Programmers/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+select a.animal_id, a.animal_type, a.name
+from animal_ins a inner join animal_outs b on a.animal_id = b.animal_id 
+where a.SEX_UPON_INTAKE like '%Intact%' and b.SEX_UPON_OUTCOME not like '%Intact%'
+order by a.animal_id
