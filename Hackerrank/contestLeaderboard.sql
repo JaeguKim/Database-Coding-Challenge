e0d4bb32f2a56608d52b4efc6d150fd8495072d4
김재구 <rlaworn1993@gmail.com>
2020-10-05 17:52:52 +0900

    10.05

--- Hackerrank/contestLeaderboard.sql
+++ Hackerrank/contestLeaderboard.sql
@@ -10,3 +10,11 @@ FROM
     HAVING SUM(SCORE) <> 0) AS TOTAL_SCORE_TABLE, HACKERS H
 WHERE TOTAL_SCORE_TABLE.HACKER_ID = H.HACKER_ID
 ORDER BY TOTAL_SCORE DESC, H.HACKER_ID
+
+#SOLUTION 2
+SELECT m.hacker_id, h.name, SUM(m.score) AS total_score FROM
+(SELECT hacker_id, challenge_id, MAX(score) AS score FROM Submissions GROUP BY hacker_id, challenge_id) AS m
+JOIN Hackers AS h ON m.hacker_id = h.hacker_id 
+GROUP By m.hacker_id, h.name
+HAVING total_score > 0
+ORDER BY total_score DESC, m.hacker_id;
